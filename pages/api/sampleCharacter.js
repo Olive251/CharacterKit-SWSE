4feb3dd9aed07309ff39de3e0bcb300ebe8f4429
Olivia Brown <oliviataylor437@gmail.com>
2022-11-19 21:31:29 -0500

    bug fixing

--- pages/api/sampleCharacter.js
+++ pages/api/sampleCharacter.js
@@ -1,18 +1,24 @@
 import SwseDB from '../../lib/swseDB'
 
 let db = new SwseDB();
-db.initialize(process.env.DB_CONN).catch((err) => {
-    console.log("Unable to connect to DB");
-    console.log(err);
-})
+const DBC = process.env.DB_CONN
 
-export default function handler(req, res) {
+
+export default async function handler(req, res) {
+
+    await db.initialize(DBC).catch((err) => {
+        console.log("Unable to connect to DB");
+        console.log(err);
+    });
+
     try {
-        db.getCharacterById('637861cb8b05c74005dd0dae')
+        await db.getCharacterById('637861cb8b05c74005dd0dae')
         .then((data) => {
+            console.log(data);
           res.status(200).json(data);
       })
     } catch (error) {
+        console.log(error)
           res.status(400).json({error: error.message})
     }
 }
